577b529546e6f5ff2b7070153936ffb4c2fdc951
Francesco Davide <francescodavide189@gmail.com>
2025-06-03 14:43:17 +0200

    indedx renamed as index and other 2 changes

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-console.log("Hello World");
-
 const http = require ('http');
-http.createServer(function (req,res=){
+http.createServer(function (req,res){
     res.writeHead(200,
         {'Content-Type': 'text/plain'});
     res.write('Hello world!!');
--- test2.js
+++ test2.js
@@ -1,5 +1,5 @@
 const http = require ('http');
-http.createServer(function (req,res=){
+http.createServer(function (req,res){
     res.writeHead(200,
         {'Content-Type': 'text/plain'});
     res.write('Hello world!!');
