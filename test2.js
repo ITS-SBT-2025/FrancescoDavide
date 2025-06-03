a47776c2749aeb0851c3a8d201906ffc99c4e447
Francesco Davide <francescodavide189@gmail.com>
2025-06-03 14:35:22 +0200

    three new files for exercises

--- test2.js
+++ test2.js
@@ -1,5 +1,3 @@
-console.log("Hello World");
-
 const http = require ('http');
 http.createServer(function (req,res=){
     res.writeHead(200,
@@ -7,5 +5,3 @@ http.createServer(function (req,res=){
     res.write('Hello world!!');
     res.end();
 }).listen(8080);
-//the metod in line 4 require a function
-//line 5 there is the function
